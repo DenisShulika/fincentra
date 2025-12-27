5ecaf60fb321734fa4841632918d99f3fc512f85
Denis Shulika <denisshulika31@gmail.com>
2025-12-27 22:50:40 +0200

    Changed tragetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.denisshulika.fincentra"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.denisshulika.fincentra"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
