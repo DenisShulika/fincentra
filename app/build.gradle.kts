7d4724b6bd7e81248c27ad5a2c4d632f20891d00
Denis Shulika <denisshulika31@gmail.com>
2026-05-11 02:43:15 +0300

    Changed app version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.denisshulika.fincentra"
         minSdk = 31
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
